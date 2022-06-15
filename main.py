e9b03c66ac0aed4506fbd27cde66936c259831f6
Rumetta-may <achmad7actaca@gmail.com>
2022-06-15 07:21:54 +0700

    direct token

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def on_guild_channel_create(channel):
 
 print(col_sett['channel'])
 """run the bot with token from env"""
-bot.run('OTcxOTQ1NjI4MTY5MDExMjQw.GmQjH-.swk7vCTlcPoDS2Ygymef9J0a_mfKm2PzW6MUvg')
+bot.run('OTcxOTQ1NjI4MTY5MDExMjQw.GCCMZA.p3nUvV9h4I46MXbd-6A3C47ATuGafRiSUIYmmU')
 
 
 
